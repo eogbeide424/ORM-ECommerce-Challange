cc0959d55e9fc5b5fec259a13b1d9d0b263940e8
unknown <eogbeide2@gmail.com>
2023-05-04 22:14:45 -0700

    finshed product routes and tested

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,8 +37,8 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
  try{
-  const categoryData = await Category.create({
-    category_name: req.body.category_name,
+  const categoryData = await Category.create(req.body,{
+    category_name: req.body,
   });
   res.status(200).json(categoryData);
  } catch (err) {
